2223cabcf6b1d595fd6280a9655fab1fb3e4c902
derekolson09 <d.g.olson09@gmail.com>
2019-07-09 02:12:10 -0700

    create(appinit): Created the first version of app

---
+++ src/components/users/User.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useContext, Fragment } from 'react';
+import Spinner from '../layout/Spinner';
+import Repos from '../repos/Repos';
+import { Link } from 'react-router-dom';
+import GithubContext from '../../context/github/githubContext';
+
+
+
+const User = ({ match }) => {
+
+    const githubContext = useContext(GithubContext);
+
+    const { getUserRepos, repos, getUser, loading, user } = githubContext
+
+    //Empty bracket prevents loop upload (mimics behaviour of component did mount)
+    useEffect(() => {
+        getUser(match.params.login);
+        getUserRepos(match.params.login);
+        //eslint-disable-next-line
+    }, []);
+
+
+
+    const {
+        name,
+        avatar_url,
+        location,
+        bio,
+        company,
+        blog,
+        login,
+        html_url,
+        followers,
+        following,
+        public_repos,
+        public_gists,
+        hireable
+    } = user;
+
+    if (loading) return <Spinner />
+    return (
+        <Fragment>
+            <Link to='/' className='btn btn-light'>
+                Back to search
+            </Link>
+            Hireable: {' '} {hireable ? <i className="fas fa-check text-succes" /> : <i className="fas fa-times-circle text-danger" />}
+            <div className="card grid-2">
+                <div className="all-center">
+                    <img src={avatar_url} className="round-img" alt="" style={{ width: '150px' }} />
+                    <h1>{name}</h1>
+                    <p>Location: {location ? location : 'Not available'}</p>
+                </div>
+                <div>
+                    {bio && <Fragment>
+                        <h3>Bio</h3>
+                        <p>{bio}</p>
+                    </Fragment>}
+                    <a href={html_url} className="btn btn-dark my-1"> Visit Github Profile </a>
+                    <ul>
+                        <li>
+                            {login && <Fragment>
+                                <strong>Username: </strong> {login}
+                            </Fragment>}
+                        </li>
+                        <li>
+                            {company && <Fragment>
+                                <strong>Company: </strong> {company}
+                            </Fragment>}
+                        </li>
+                        <li>
+                            {blog && <Fragment>
+                                <strong>Website: </strong> {blog}
+                            </Fragment>}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="card text-center">
+                <div className="badge badge-primary">Followers: {followers}</div>
+                <div className="badge badge-success">Following: {following}</div>
+                <div className="badge badge-light">Public Repos: {public_repos}</div>
+                <div className="badge badge-dark">Public Gists: {public_gists}</div>
+            </div>
+            <Repos repos={repos} />
+        </Fragment>
+    )
+}
+
+export default User
